010994c223ac0519479ad74066ff93ecd2d954d9
vlademirfaria <vemir38@hotmail.com>
2024-09-07 13:54:06 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,31 +3,24 @@ let campoPesquisa = document.getElementById("campo-pesquisa");
 
 // Adiciona um ouvinte de evento para o pressionamento de tecla no campo de pesquisa
 campoPesquisa.addEventListener("keydown", function(event) {
-  if (event.keyCode === 13) {
-    event.preventDefault(); 
-    pesquisar();
+  if (event.keyCode === 13) { // Verifica se a tecla pressionada é Enter (código 13)
+    event.preventDefault(); // Impede o comportamento padrão do Enter (recarregar a página)
+    pesquisar(); // Chama a função de pesquisa
   }
 });
 
 function pesquisar() {
-  // Obtém a seção onde os resultados da pesquisa serão exibidos
   let section = document.getElementById("resultados-pesquisa");
-
-  // Obtém o valor do campo de pesquisa, remove acentos e converte para minúsculas
   let campoPesquisa = document.getElementById("campo-pesquisa").value.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
 
-  // Verifica se o campo de pesquisa está vazio ou contém apenas caracteres especiais
   if (campoPesquisa.trim() === "" || !/[a-zA-Z0-9]/.test(campoPesquisa)) {
     section.innerHTML = ""; 
     return; 
   }
 
-  // Inicializa a variável que armazenará o HTML dos resultados
   let resultados = "";
 
-  // Itera sobre os dados e constrói o HTML para cada resultado
   for (let carta of cartas) {
-    // Normaliza e converte para minúsculas o nome, tipo e descrição da carta
     let nome = carta.nome.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
     let descricao = carta.descricao.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
     let tipo = carta.tipo.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase(); 
@@ -51,7 +44,35 @@ function pesquisar() {
     }
   }
 
-  // Verifica se algum resultado foi encontrado
+  if (resultados === "") {
+    section.innerHTML = "<p>Nada foi encontrado</p>";
+  } else {
+    section.innerHTML = resultados;
+  }
+}
+
+function mostrarTodas() {
+  let section = document.getElementById("resultados-pesquisa");
+  let resultados = "";
+
+  for (let carta of cartas) {
+    let ataqueExibido = carta.ataque !== null ? carta.ataque : 0;
+    let defesaExibida = carta.defesa !== null ? carta.defesa : 0;
+
+    resultados += `
+      <div class="item-resultado"> 
+          <h2>${carta.nome}</h2>
+          <p class="descricao-meta">
+              <b>Tipo da carta:</b> ${carta.tipo}<br>
+              <b>Ataque: </b> ${ataqueExibido}<br> 
+              <b>Defesa: </b> ${defesaExibida}<br> 
+              <b>Descrição da carta: </b> ${carta.descricao}
+          </p>
+          <a href="https://pt.wikipedia.org/wiki/Yu-Gi-Oh!" target="_blank">O que é o Yu-gi-oh?</a>
+      </div>
+    `;
+  }
+
   if (resultados === "") {
     section.innerHTML = "<p>Nada foi encontrado</p>";
   } else {
